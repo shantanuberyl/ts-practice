0f7d6639d76b2a50ec67d1e18b8718176d079caf
shantanurana7 <shantanu.rana7@gmail.com>
2018-04-03 12:36:12 +0530

    type assertions

---
+++ type_assertion.js
@@ -0,0 +1,3 @@
+var any;
+any = 'abc';
+var insertD = any.endsWith('D'); /** inster <any> to add type assertion  */
---
+++ type_assertion.ts
@@ -0,0 +1,5 @@
+let any;
+
+any = 'abc';
+
+let insertD = (<string>any).endsWith('D');  /** inster <any> to add type assertion  */
