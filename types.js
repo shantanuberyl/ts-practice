928f3054700dc01408c4a121e1ee305db1c8263f
shantanurana7 <shantanu.rana7@gmail.com>
2018-04-03 12:24:34 +0530

    types in typscript

---
+++ types.js
@@ -0,0 +1,15 @@
+var a;
+a = 3;
+a = true;
+a = 'hello';
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Blue"] = 1] = "Blue";
+    Color[Color["Green"] = 2] = "Green";
+})(Color || (Color = {}));
+;
+var b;
+var c;
+var d;
+var e;
---
+++ types.ts
@@ -0,0 +1,11 @@
+let a:any;
+a = 3;
+a = true;
+a = 'hello';
+
+enum Color {Red = 0, Blue = 1, Green = 2};
+
+let b: number;
+let c: boolean;
+let d: string;
+let e: number[]; 
